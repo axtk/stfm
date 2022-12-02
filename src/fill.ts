8af389c3d3c7c03ff5fbafe2f19ae1b285206027
axtk <axtkx@inbox.ru>
2022-12-03 02:40:33 +0300

    export fill transformMap type

--- src/fill.ts
+++ src/fill.ts
@@ -1,13 +1,15 @@
 import {escapeRegExp} from './escapeRegExp';
 
-export function fill<T extends Record<string, unknown>>(
+export type FillTransformMap<K extends string, T> = Record<K, (data: T) => unknown>;
+
+export function fill<K extends string, T extends Record<K, unknown>>(
     template: string,
     data: T,
-    transformMap?: Record<string, (data: T) => unknown>,
+    transformMap?: FillTransformMap<K, T>,
 ): string {
     let s = template;
 
-    for (let [key, value] of Object.entries(data))
+    for (let [key, value] of Object.entries(data) as Array<[K, unknown]>)
         s = s.replace(
             new RegExp(`\\{${escapeRegExp(key)}\\}`, 'g'),
             String(transformMap?.[key] ? transformMap[key](data) : value),
