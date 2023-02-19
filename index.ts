3cc61c060930ea5113e9f2641e34c544d432ce27
axtk <axtkx@inbox.ru>
2023-02-19 20:32:28 +0300

    add toRoman

--- index.ts
+++ index.ts
@@ -5,4 +5,5 @@ export * from './src/escapeRegExp';
 export * from './src/getRandomString';
 export * from './src/fill';
 export * from './src/pad';
+export * from './src/toRoman';
 export * from './src/unescapeHTML';
---
+++ src/toRoman.ts
@@ -0,0 +1,31 @@
+const map: [string, number][] = [
+    ['M', 1000],
+    ['CM', 900],
+    ['D', 500],
+    ['CD', 400],
+    ['C', 100],
+    ['XC', 90],
+    ['L', 50],
+    ['XL', 40],
+    ['X', 10],
+    ['IX', 9],
+    ['V', 5],
+    ['IV', 4],
+    ['I', 1],
+];
+
+export function toRoman(x: number): string {
+    if (typeof x !== 'number')
+        return '';
+    
+    let output = '';
+
+    for (let [r, a] of map) {
+        while (x >= a) {
+            output += r;
+            x -= a;
+        }
+    }
+    
+    return output;
+}
